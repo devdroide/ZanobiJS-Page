a751348e4fdef9cb7f7493fbb3293d5686fbe2be
devdroide <johne.aplicativos@gmail.com>
2024-02-11 12:32:00 -0500

    :sparkles: ZNB-001: deploy added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "zanobijs.github.io",
   integrations: [
     starlight({
       title: "ZanobiJS",
